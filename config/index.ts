8c725d4e47fa1226a891e51efaa866d011290fa7
Yu Mao <maoyulore@outlook.com>
2025-01-16 19:10:28 +0800

    Update API endpoint for production environment

--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,4 @@
 export const endpoint =
   process.env.NODE_ENV === 'production'
-    ? 'https://api.maoyu.space'
+    ? 'https://api.maoyu.space/api/v1/poem'
     : 'http://localhost:4000/api/v1/poem';
